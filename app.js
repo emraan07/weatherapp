c16495d980f1411aa940326dbb4d129b7666b8a4
Imran khan <imkhang196@gmail.com>
2022-08-20 23:23:23 +0400

    correct app.listen code

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.post("/", function(req, res) {
     })
     
 })
-app.listen(process.env.port || 4000, function(req, res) {
+app.listen(process.env.port || 4000, function() {
     console.log("Server started running on port 4000");
 })
